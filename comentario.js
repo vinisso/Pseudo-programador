343810261646062676690eb9d8e830db98a0926a
Vinicius Pereira <66280684+bochauu@users.noreply.github.com>
2020-06-24 14:45:37 -0300

    Adicionando códigos
    Algumas bases de JS

---
+++ comentario.js
@@ -0,0 +1,17 @@
+// comentário de uma linha
+console.log('Linha 1')
+
+/* Comentário
+de
+multiplas
+linhas */
+console.log('Linha 2')
+
+/*
+ * Comentário de 
+ * Multiplas
+ * linhas
+ * estilizado...
+ */
+
+console.log('Linhas 3')
