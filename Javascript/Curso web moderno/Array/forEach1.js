fe96b0c98ba668653145fc826ae7f16593d35ed0
bochau <60614072+bochau@users.noreply.github.com>
2020-10-22 08:40:04 -0300

    sim

--- Javascript/Curso web moderno/Array/forEach1.js
+++ Javascript/Curso web moderno/Array/forEach1.js
@@ -3,3 +3,10 @@ const aprovados = ['Agahta', 'Aido', 'Daniel', 'Raquel']
 aprovados.forEach(function(nome, indice) {
     console.log(`${indice + 1}) ${nome}`)
 })
+
+aprovados.forEach(nome => console.log(nome))
+
+//Passando função por paramêtro
+
+const exibirAprovados = aprovado => console.log(aprovado)
+aprovados.forEach(exibirAprovados)
