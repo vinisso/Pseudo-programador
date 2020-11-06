6ba50ffd4376e336ea4f12b58f02b7957e995869
Vinicius Pereira <66280684+bochauu@users.noreply.github.com>
2020-11-06 15:09:20 -0300

    Att+

--- Javascript/Curso web moderno/Array/map3.js
+++ Javascript/Curso web moderno/Array/map3.js
@@ -1,3 +1,11 @@
+Array.prototype.map2 = function(callback) {
+    const newArray = []
+    for (let i = 0; i < this.length; i++ ) {
+        newArray.push(callback(this[i], i, this))
+    }
+    return newArray
+}
+
 const carrinho = [
     '{ "nome": "Borracha", "preco": 3.45 }',
     '{ "nome": "caderno", "preco": 13.90 }',
@@ -10,5 +18,5 @@ const carrinho = [
 const paraObjeto = json => JSON.parse(json)
 const apenasPreco = produto => 'R$ ' + produto.preco
 
-const resultado = carrinho.map(paraObjeto).map(apenasPreco)
+const resultado = carrinho.map2(paraObjeto).map2(apenasPreco)
 console.log(resultado)
