1023d9296d90cb86b4f45e444bc7f5540f217226
Vinicius Pereira <vincenslau@gmail.com>
2021-12-02 09:23:28 -0300

    h

---
+++ Javascript/Curso web moderno/node/moduloA.js
@@ -0,0 +1,6 @@
+const moduloB = require("./moduloB")
+
+this.ola = 'Oi'
+exports.bemVindo = 'Bem vindo'
+module.exports.ateLogo = 'Tchau'
+
---
+++ Javascript/Curso web moderno/node/moduloCliente.js
@@ -0,0 +1,17 @@
+const moduloA = require('./moduloA')
+const moduloB = require('./moduloB')
+
+console.log('Modulo A \n')
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.ateLogo)
+
+
+console.log('\n')
+console.log(moduloA)
+console.log('\n\n')
+
+console.log('Modulo B \n')
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
+console.log(moduloB)
